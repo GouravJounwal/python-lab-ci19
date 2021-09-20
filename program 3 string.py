93029fa52b126f73bc921f2a43fd7123a5dc68ee
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2021-09-20 18:27:02 +0530

    Create program 3 string.py

---
+++ program 3 string.py
@@ -0,0 +1,10 @@
+var1 = 'Hello World'
+var2 = "Hello Everyone welcome"
+
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[6:10])
+
+var1 = 'Hello World'
+print ("Updated String : ", var1[:6] + 'Python')
+
+print ("this is %s and age is %d kg" % ('Gourav', 19))
