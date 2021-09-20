9fea613df5e8ff2d63136c9e27e1e42e32575cae
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2021-09-20 18:02:12 +0530

    Create program 2 tuple.py

---
+++ program 2 tuple.py
@@ -0,0 +1,17 @@
+tup = ('python', 'Gourav') 
+
+print(tup) 
+tuple1 = (0, 1, 2, 3) 
+
+tuple2 = ('python', 'Gourav') 
+
+print(tuple1 + tuple2) 
+tuple3 = ('python',)*3  
+
+print(tuple3) 
+list1 = [0, 5, 2] 
+
+print(tuple(list1)) 
+# string 'python' 
+
+print(tuple('python')) 
