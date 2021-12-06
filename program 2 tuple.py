69b7f0b2693c1de6d421edcfd46f64d1bee8e631
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2021-12-06 12:21:01 +0530

    Update program 2 tuple.py

--- program 2 tuple.py
+++ program 2 tuple.py
@@ -15,3 +15,45 @@ print(tuple(list1))
 # string 'python' 
 
 print(tuple('python')) 
+#1.	 Declaring of tuple
+
+tup = ('python', 'Gourav') 
+
+print(tup) 
+tuple1 = (0, 1, 2, 3) 
+
+tuple2 = ('python', 'Gourav') 
+
+print(tuple1 + tuple2) 
+tuple3 = ('python',)*3  
+
+print(tuple3) 
+list1 = [0, 5, 2] 
+
+#2.	count number of occurrence
+
+tuple1.count(2)
+
+output-: 2
+
+#3.	find first occurrence of a particular element
+
+tuple1.index(1)
+
+Output -:1
+
+
+#4.	Creating an empty tuple
+
+my_tuple = ()
+print(my_tuple)
+
+
+#5.	Creating an empty tuple
+
+my_tuple = ()
+print(my_tuple)
+
+
+
+
